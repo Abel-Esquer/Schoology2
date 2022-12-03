04523d9f7fa695f64ae66d7bd3f508578402919e
CH1L0W <maritooo02@hotmail.com>
2022-12-02 23:51:33 -0700

    Se completo el CRUD de Usuario

--- Schoology2.Core/Entidades/Usuario.cs
+++ Schoology2.Core/Entidades/Usuario.cs
@@ -44,7 +44,7 @@ namespace Schoology2.Core.Entidades
                         usuario.Nombre = dataReader["nombre"].ToString();
                         usuario.Apellido = dataReader["apellido"].ToString();
                         usuario.Correo = dataReader["correo"].ToString();
-                        usuario.Contraseña = dataReader["password"].ToString();
+                        usuario.Contraseña = dataReader["contraseña"].ToString();
                         usuario.Rol = dataReader["rol"].ToString();
 
                         usuarios.Add(usuario);
@@ -61,8 +61,109 @@ namespace Schoology2.Core.Entidades
             }
             return usuarios;
         }
+       
+        //Agregar y Editar
+        public static bool Guardar (int id, string nombre, string correo, string apellido, string contraseña, string rol)
+        {
+            bool result = false;
+            try
+            {
+                Conexion conexion = new Conexion();
+                if (conexion.OpenConnection())
+                {
+                    MySqlCommand cmd = conexion.connection.CreateCommand();
 
+                    if (id == 0)
+                    {
+                        cmd.CommandText = "INSERT INTO usuario (nombre, apellido, correo, contraseña, rol) VALUES (@nombre, @apellido, @correo, @contraseña, @rol";
+                        cmd.Parameters.AddWithValue("@nombre", nombre);
+                        cmd.Parameters.AddWithValue("@apellido", apellido);
+                        cmd.Parameters.AddWithValue("@correo", correo);
+                        cmd.Parameters.AddWithValue("@contraseña", contraseña);
+                        cmd.Parameters.AddWithValue("@rol", rol);
+                    }
+                    else
+                    {
+                        cmd.CommandText = "UPDATE usuario SET nombre = @nombre, apellido = @apellido, correo = @correo, contraseña = @contraseña, rol = @rol WHERE id = @id" +
+                        cmd.Parameters.AddWithValue("@id", id);
+                        cmd.Parameters.AddWithValue("@nombre", nombre);
+                        cmd.Parameters.AddWithValue("@apellido", apellido);
+                        cmd.Parameters.AddWithValue("@correo", correo);
+                        cmd.Parameters.AddWithValue("@contraseña", contraseña);
+                        cmd.Parameters.AddWithValue("@rol", rol);
+                    }
+                    result = cmd.ExecuteNonQuery() == 1;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return result;
+        }
 
+        public static bool Eliminar(int id)
+        {
+            bool result = false;
+            Usuario usuario = new Usuario();
+            try
+            {
+                Conexion conexion = new Conexion();
+                if (conexion.OpenConnection())
+                {
+
+                    MySqlCommand cmd = conexion.connection.CreateCommand();
+                    cmd.CommandText = "DELETE FROM usuario WHERE id = @id";
+
+                    cmd.Parameters.AddWithValue("@id", id);
+
+                    MySqlDataReader dataReader = cmd.ExecuteReader();
+                    dataReader.Close();
+                    conexion.CloseConnection();
+                    result = cmd.ExecuteNonQuery() == 1;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw (ex);
+            }
+            return result;
+        }
+
+        public static Usuario GetById(int id)
+        {
+            Usuario usuario = new Usuario();
+            try
+            {
+                Conexion conexion = new Conexion();
+                if (conexion.OpenConnection())
+                {
+                    string query = "SELECT * FROM usuario WHERE id = @id";
+
+                    MySqlCommand cmd = new MySqlCommand(query, conexion.connection);
+                    cmd.Parameters.AddWithValue("@id", id);
+
+                    MySqlDataReader dataReader = cmd.ExecuteReader();
+                    while (dataReader.Read())
+                    {
+                        usuario.Id = int.Parse(dataReader["id"].ToString());
+                        usuario.Nombre = dataReader["nombre"].ToString();
+                        usuario.Apellido = dataReader["apellido"].ToString();
+                        usuario.Correo = dataReader["correo"].ToString();
+                        usuario.Contraseña = dataReader["contraseña"].ToString();
+                        usuario.Rol = dataReader["rol"].ToString();
+                    }
+
+                    dataReader.Close();
+                    conexion.CloseConnection();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return usuario;
+        }
     }
     
 }
